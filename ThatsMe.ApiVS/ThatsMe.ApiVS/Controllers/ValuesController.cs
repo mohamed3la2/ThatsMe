d2c1bf3f8cb4479828017d94baf3ac625251eed4
Mohamed Alaa <Mohamed.3la2.fciscu@gmail.com>
2020-06-12 09:43:28 +0200

    init commit

---
+++ ThatsMe.ApiVS/ThatsMe.ApiVS/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ThatsMe.ApiVS.Data;
+
+namespace ThatsMe.ApiVS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context )
+        {
+            _context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var res = await _context.Values.ToListAsync();
+            return Ok(res);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var res = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(res);
+        }
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetValuebyName(string name)
+        {
+            var res = await _context.Values.FirstOrDefaultAsync(x => x.Name == name);
+            return Ok(res);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ThatsMe.ApiVS/ThatsMe.ApiVS/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThatsMe.ApiVS.Models;
+
+namespace ThatsMe.ApiVS.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option):base(option)
+        {       
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
